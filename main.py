8aee83cd4393a583c76284dd8e3e30722de1f537
Psyrus <revolution.x@gmail.com>
2017-12-03 19:52:14 +0900

    Added csv parsing and evaluation. Cleaned up UI

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 def enable_local_error_handling():
     app.logger.addHandler(logging.StreamHandler())
     app.logger.setLevel(logging.INFO)
-    
+
 
 @app.route('/form')
 def form():
@@ -43,24 +43,59 @@ def how_to():
 @app.route('/fps-submitted', methods=['POST'])
 def fps_submitted():
     form = FpsForm(request.form)
-    if request.method == "POST" and form.validate():
-        cpu = form.cpu.data
-        gpu = form.gpu.data
-        ram = form.gpu.data
-        csv_data = []
-        if request.files:
-            print("Values in fps_values!")
-            print(request.files)
-            print(form.fps_values.name)
-            fileobj = request.files[form.fps_values.name]
-            print(fileobj)
-            if not fileobj.mimetype  == "text/csv":
-                print("This is not a csv!!!")
-            else:
-                print("It's a csv")
-                stream = io.StringIO(fileobj.read().decode("UTF8"), newline=None)
-                csv_input = csv.reader(stream)
-                csv_data = list(csv_input)
-        avg = 0
-        return render_template("submitted_form.html", cpu=cpu, gpu=gpu, ram=ram, avg=avg, csv_data=csv_data)
-    return "{}".format(form.cpu.data)
+
+    #Exit if the form is not valid
+    if not request.method == "POST" or not form.validate():
+        return render_template("submitted_form.html", error_string = "The form data was invalid")
+
+    # Capture base data from the form
+    cpu = form.cpu.data
+    gpu = form.gpu.data
+    ram = form.ram.data
+    csv_data = []
+
+    #Exit if no files were uploaded
+    if not request.files:
+        return render_template("submitted_form.html", error_string = "No file was uploaded")
+
+    fileobj = request.files[form.fps_values.name]
+
+    #Exit if the uploaded file was not a CSV
+    if not fileobj.mimetype  == "text/csv":
+        return render_template("submitted_form.html", error_string = "The uploaded file was not a CSV file")
+
+    #Convert uploaded file to a list of strings for processing
+    stream = io.StringIO(fileobj.read().decode("UTF8"), newline=None)
+    csv_input = csv.reader(stream)
+    csv_data = list(csv_input)[1:]
+    avg = 0
+
+    #Initialize a simple min_val/max_val to use for comparisons in the loop
+    min_val = int(csv_data[0][0])
+    max_val = min_val
+
+    invalid_lines = 0
+    #Loop through to establish average, min and max fps
+    for item in csv_data:
+        if not item:
+            invalid_lines += 1
+            continue
+        item = int(item[0])
+        avg += item
+        max_val = item if max_val < item else max_val
+        min_val = item if min_val > item else min_val
+
+    valid_data_count = len(csv_data) - invalid_lines
+    avg = float(avg) / valid_data_count
+
+    #Loop through a second time to establish the average standard deviation
+    total_diff_from_avg = 0
+    for item in csv_data:
+        if not item:
+            continue
+        total_diff_from_avg += abs(int(avg) - int(item[0]))
+
+    std_dev = total_diff_from_avg / valid_data_count
+
+    #Should make a view model to clean up the render_template call
+    return render_template("submitted_form.html", cpu=cpu, gpu=gpu, ram=ram, avg=avg, std_dev = std_dev, max_val = max_val, min_val = min_val, csv_data=csv_data)
---
+++ models.py
@@ -0,0 +1,14 @@
+# Models used in the program
+
+# CPU model
+
+# GPU model
+
+# RAM model
+
+# PC config model (GPU/CPU/RAM/Resolution)
+### DXDIAG regex (english)
+# Processor: (.*GHz).*?\n.*?Memory: (.*?) RAM[\s\S]*?Chip type: (.*?)\n[\s\S]*?Native Mode: (\d+ x \d+).*?\n
+###
+
+# Submission model (PC hash, squad version, map, max, min std dev)
